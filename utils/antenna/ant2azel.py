c67d6b166fc4dc794a6a64649c1d7c5a1d9f7f51
Darcy Buskermolen <darcy@dbitech.ca>
2019-10-28 11:58:37 -0700

    Fix db normaization as per https://github.com/Cloud-RF/Signal-Server/pull/20

--- utils/antenna/ant2azel.py
+++ utils/antenna/ant2azel.py
@@ -6,11 +6,7 @@ from os.path import splitext
 
 
 def db_to_norm(db):
-<<<<<<< HEAD
-    return 10**(db/10.)
-=======
     return 10**(db/20.)
->>>>>>> b9ca4186fe59882c5b291955e76a1d664f4aece9
 
 
 def main(argv):
@@ -24,13 +20,13 @@ def main(argv):
     try:
         opts, args = getopt.getopt(argv,"dha:i:o:t:",["debug","help","azimuth=","infile=","outfile=","tilt="])
     except getopt.GetoptError:
-        print program + '  --debug --help -a <azimuth> -t <mechanicaldowntilt> -i <inputfile> -o <outputfile>'
+        print (program + '  --debug --help -a <azimuth> -t <mechanicaldowntilt> -i <inputfile> -o <outputfile>')
         sys.exit(2)
     for opt, arg in opts:
         if opt in ("-d", "--debug"):
             debug = 1
         elif opt in ("-h", "--help"):
-            print  program + ' --debug --help -a <azimuth> -t <mechanicaldowntilt> -i <inputfile> -o <outputfile>'
+            print  (program + ' --debug --help -a <azimuth> -t <mechanicaldowntilt> -i <inputfile> -o <outputfile>')
             sys.exit(2)
         elif opt in ("-i", "--infile"):
             antfile = arg
@@ -41,19 +37,19 @@ def main(argv):
         elif opt in ("-t", "--tilt"):
             tilt = arg
         if antfile == '':
-            print  program + ' --debug --help -a <azimuth> -t <mechanicaldowntilt> -i <inputfile> -o <outputfile>'
+            print  (program + ' --debug --help -a <azimuth> -t <mechanicaldowntilt> -i <inputfile> -o <outputfile>')
             sys.exit(2)
     if outfile == '' :
         outfile = splitext(antfile)[0]
     if debug :
-        print "outfile " + outfile
-        print "downtilt " + tilt     
+        print ("outfile " + outfile)
+        print ("downtilt " + tilt)  
     with open(antfile, 'r') as ant:
         with open(outfile + '.az', 'w') as az:
             # azimuth offset as provided by command arg or 0
             az.write("%d\n" % float(direction))
             # Read the first 360 lines of the file
-            for i in xrange(360):
+            for i in range(360):
                 az.write("%d\t%0.4f\n" % (i, db_to_norm(float(next(ant)))))
         with open(outfile + '.el', 'w') as el:
             # mechanical downtilt, azimuth of tilt
